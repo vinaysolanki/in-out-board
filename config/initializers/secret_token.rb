b4a2125070aa6a85e6354f3f9ef2b41c50d04324
vinay <vinaysolanki2005@gmail.com>
2013-04-14 02:21:34 -0700

    new in/out board app and first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InOutBoard::Application.config.secret_token = 'cc9e1f697e7a1dd4751052b0165e109904d3d73f48cd90d020e3e4beb3223fd014561858fcbfdcbdf34b3ddfd4d4971570b797347d127de29fde1fd0765ac0f3'
