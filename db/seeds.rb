a7ab6670f7292e5f5dfacbcfc78ec58d2d42bcf8
vinay <vinaysolanki2005@gmail.com>
2013-04-14 20:01:11 -0700

    added seeds users and removed unwanted migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: 'nickstijne@gmail.com', password: 'asdasdasd',password_confirmation: 'asdasdasd', fullname: 'Nick Stijne')
+User.create(email: 'johndoe@gmail.com', password: 'asdasdasd',password_confirmation: 'asdasdasd', fullname: 'John Doe')
+User.create(email: 'alisonmay@gmail.com', password: 'asdasdasd',password_confirmation: 'asdasdasd', fullname: 'Alison May')
+User.create(email: 'anjalirai@gmail.com', password: 'asdasdasd',password_confirmation: 'asdasdasd', fullname: 'Anjali Rai')
+User.create(email: 'vinaysolanki@gmail.com', password: 'asdasdasd',password_confirmation: 'asdasdasd', fullname: 'Vinay Solanki')
+User.create(email: 'alexleung@gmail.com', password: 'asdasdasd',password_confirmation: 'asdasdasd', fullname: 'Alex Leung')
+User.create(email: 'vincentchu@gmail.com', password: 'asdasdasd',password_confirmation: 'asdasdasd', fullname: 'Vincent Chu')
+User.create(email: 'brucewayne@gmail.com', password: 'asdasdasd',password_confirmation: 'asdasdasd', fullname: "Bruce 'Batman' Wayne")
+User.create(email: 'clarkkent@gmail.com', password: 'asdasdasd',password_confirmation: 'asdasdasd', fullname: "Clark 'Superman' Kent")
+User.create(email: 'dianaprince@gmail.com', password: 'asdasdasd',password_confirmation: 'asdasdasd', fullname: "Diana 'Wonder Woman' Prince")
