11405d1dd614d7f30e480fac4ad565d9e9821c9a
vinay <vinaysolanki2005@gmail.com>
2013-04-14 17:31:40 -0700

    added change and update methods for user status

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -3,5 +3,35 @@ class BoardController < ApplicationController
   
     @users = User.all
     
+    if user_signed_in?
+      current_user.status == 'Online'
+    end
+  
   end
+  
+  def change
+    @user = current_user
+  end
+  
+  def update
+    @user = current_user
+    @user.status = params[:user][:status]
+    @user.save
+    redirect_to :root
+  end
+  
+  
+  def status_label
+  
+    if current_user.status == 'Not In'
+      @label = 'label-important'
+    elsif current_user.status == 'Away'
+      @label = 'label-warning'
+    else
+      @label = 'label-success'
+    end
+  
+  end
+  
+  
 end
